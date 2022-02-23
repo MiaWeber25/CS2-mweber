47dc9a3b35c573b8265ea0b49a707a20da367c19
Mia Weber <miaweber02@gmail.com>
2022-02-22 20:30:44 -0700

    working parsing algorithm

--- semesterProject/testing.cpp
+++ semesterProject/testing.cpp
@@ -9,6 +9,7 @@ using namespace std;
 int main() {
     vector<string> line; //vector of integers to store the pre-parsed data from test.txt
     vector<string> tokens;
+    vector<string> tokens2;
     string inFile;
     string temp;
     cout << "enter file name: ";
@@ -16,6 +17,7 @@ int main() {
     ifstream fin;
     fin.open(inFile, ios::in);
     string intermediate;
+    string intermediate2;
 
     while(!fin.eof()) {
         fin >> temp;
@@ -23,115 +25,19 @@ int main() {
     } 
     for (int t=0; t<line.size(); t++) {
         stringstream ss(line[t]);
-        while(getline(ss, intermediate, ':')) {
+        while(getline(ss, intermediate, ',')) {
             tokens.push_back(intermediate);
         }
     }
-    for (int i=0; i<tokens.size(); i++) {
-        cout << tokens[i] << endl;
-    }
-
-/*
-    //all the code above does is read data into temp and then add temp to line vector
-    //code below attempts to then parse that data... doesn't work, and still doesn't account for ','
-    for(int i=0; i<line.size(); i++) {
-        stringstream ss(line[i]);
-        while(getline(ss,intermediate, ':')) {
-            tokens.push_back(intermediate);
+    for (int t=0; t<tokens.size(); t++) {
+        stringstream sss(tokens[t]);
+        while(getline(sss, intermediate2, ':')) {
+            tokens2.push_back(intermediate2);
         }
     }
-
-     This code seems to be working when the data values are assigned to input rather than read from a file. 
-    Attempt to combine this logic with file input is above
-
-    vector<string> firstValue;
-    vector<string> secondValue;
-
-    string input = "1:1, 2:2, 3:3";
-    istringstream ss(input);
-    string token;
-
-    while (getline(ss, token, ',')) {
-        while(getline(ss, token, ':'))
-            cout << token << endl;
-    }*/
-
-    return 0;
-}
-
-
-/*This code is working to read in from a file in form "2:3" or "1:2"*/
-int readFromFile() {
-    string inFile;
-    vector<string> line;
-    string temp;
-    cout << "enter name of file to read from: ";
-    cin >> inFile;
-    ifstream fin;
-    fin.open(inFile, ios::in);
-    while(!fin.eof()) {
-        fin >> temp;
-        line.push_back(temp);
-    }
-    for (int i=0; i<line.size(); i++)
-        cout << "line: " << line[i] << endl;
-    return 0; 
-}
-/*
-int main()
-{
-    //file io into a vector then use stringstream to parse each string in the vector?
-    string inFile;
-    vector<string> line;
-    string temp;
-    string the;
-    string token;
-    cout << "enter the name of the file to read from: ";
-    cin >> inFile;
-    ifstream fin;
-    fin.open(inFile, ios::in);
-    istringstream theStream(the); //put the vector in place of the?
-    while(!fin.eof()) {
-        line.push_back(temp);
-    }
-    for (int i=0; i<line.size();i++) {
-        cout << "line: " << line[i] << endl;
-    }
-
-    while(!cin.eof()) {
-        string line;
-        getline(cin,line);
-        stringstream lineStream(line);
-
-        string word;
-        while(!lineStream.eof()) {
-            getline(lineStream, word, ',');
-            cout << "[" << word << "]" << endl;
-        }
-    }
-    return 0;
-}*/
-
-
-
-
-/*
-int main() {
-    string inFile;
-    cout << "enter name of file to read from: ";
-    cin >> inFile;
-    ifstream fin;
-    fin.open(inFile, ios::in);
     
-    string line;
-    stringstream lineStream(line);
-    string temp;
-    while(!lineStream.eof()) {
-        getline(lineStream, temp, ':');
-        cout << temp;
+    for (int i=0; i<tokens2.size(); i++) {
+        cout << tokens2[i] << endl;
     }
-
     return 0;
-}*/
-
-
+}
